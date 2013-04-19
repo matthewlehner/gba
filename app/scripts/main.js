f17096f6ab1c3db39b522b51cb9ad8b76f9dd0a8
Matthew Lehner <matthewlehner@gmail.com>
2013-04-19 16:25:25 -0700

    whitespace tidy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,7 +23,6 @@ function(app, Router, FastClick) {
 
   app.router = new Router();
 
-
   // Trigger the initial route and enable HTML5 History API support, set the
   // root folder to '/' by default.  Change in app.js.
   Backbone.history.start({ pushState: true, root: app.root });
