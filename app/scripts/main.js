74512654fd30659747fd46c3b356a5aac7e9c3d6
Matthew Lehner <matthewlehner@gmail.com>
2013-05-23 18:37:42 -0700

    this now works in phone gap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,47 +5,57 @@ require([
   // Main Router.
   'router',
 
-  'FastClick'
+  'FastClick',
+
+  'domReady'
 ],
 
-function(app, Router, FastClick) {
+function(app, Router, FastClick, domReady) {
 
   // FastClick to make phone touch devices more responsive.
   new FastClick(document.body);
 
-  //Hide the address bar.
-  setTimeout(function() {
-    window.scrollTo(0,1);
-  }, 0);
+  // //Hide the address bar.
+  // setTimeout(function() {
+  //   window.scrollTo(0,1);
+  // }, 0);
 
   // Define your master router on the application namespace and trigger all
   // navigation from this instance.
-
   app.router = new Router();
 
-  // Trigger the initial route and enable HTML5 History API support, set the
-  // root folder to '/' by default.  Change in app.js.
-  Backbone.history.start({ pushState: true, root: app.root });
-
-  // All navigation that is relative should be passed through the navigate
-  // method, to be processed by the router. If the link has a `data-bypass`
-  // attribute, bypass the delegation completely.
-  $(document).on('click', 'a[href]:not([data-bypass])', function(evt) {
-    // Get the absolute anchor href.
-    var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
-    // Get the absolute root.
-    var root = location.protocol + '//' + location.host + app.root;
-
-    // Ensure the root is part of the anchor href, meaning it's relative.
-    if (href.prop.slice(0, root.length) === root) {
-      // Stop the default event to ensure the link will not cause a page
-      // refresh.
-      evt.preventDefault();
-
-      // `Backbone.history.navigate` is sufficient for all Routers and will
-      // trigger the correct events. The Router's internal `navigate` method
-      // calls this anyways.  The fragment is sliced from the root.
-      Backbone.history.navigate(href.attr, true);
-    }
+  domReady(function() {
+    document.addEventListener('deviceready', startWhenReady, false);
   });
+
+  function startWhenReady() {
+
+    // Trigger the initial route and enable HTML5 History API support, set the
+    // root folder to '/' by default.  Change in app.js.
+    Backbone.history.start({ pushState: true, root: app.root });
+
+    // All navigation that is relative should be passed through the navigate
+    // method, to be processed by the router. If the link has a `data-bypass`
+    // attribute, bypass the delegation completely.
+    $(document).on('click', 'a[href]:not([data-bypass])', function(evt) {
+      // Get the absolute anchor href.
+      var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
+      // Get the absolute root.
+      var root = location.protocol + '//' + location.host + app.root;
+
+      // Ensure the root is part of the anchor href, meaning it's relative.
+      if (href.prop.slice(0, root.length) === root) {
+        // Stop the default event to ensure the link will not cause a page
+        // refresh.
+        evt.preventDefault();
+
+        // `Backbone.history.navigate` is sufficient for all Routers and will
+        // trigger the correct events. The Router's internal `navigate` method
+        // calls this anyways.  The fragment is sliced from the root.
+        Backbone.history.navigate(href.attr, true);
+      }
+    });
+
+    app.router.index();
+  };
 });
