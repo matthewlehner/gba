4b0e31635573eb3951c0e52e05f7eb93c1c08041
Matthew Lehner <matthewlehner@gmail.com>
2013-06-02 16:32:02 -0700

    initialize this stuff on deviceReady.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,9 +12,6 @@ require([
 
 function(app, Router, FastClick, domReady) {
 
-  // FastClick to make phone touch devices more responsive.
-  new FastClick(document.body);
-
   // Define your master router on the application namespace and trigger all
   // navigation from this instance.
   app.router = new Router();
@@ -24,10 +21,14 @@ function(app, Router, FastClick, domReady) {
   });
 
   function startWhenReady() {
+    new FastClick(document.body);
+
     $(document).on('click', 'a', function(event) {
       event.preventDefault();
       window.open(event.currentTarget.href, '_blank', 'location=yes');
     });
+
+    navigator.splashscreen.hide()
 
     app.router.index();
   };
